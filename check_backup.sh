72d22e1e36f15d2cd7cc7e8df07238cc33a841ca
fc-cr <46376625+fc-cr@users.noreply.github.com>
2021-05-22 00:01:54 +0200

    Update check_backup.sh

--- check_backup.sh
+++ check_backup.sh
@@ -3,22 +3,30 @@
 #Check if hana db server
 if pgrep -x "hdbindexserver" > /dev/null
 then
-        DEXE="/usr/sap/DDB/HDB02/exe/"
     DBIN="/usr/sap/codilog/hana/bin/backup/"
     DLOG="/usr/sap/codilog/hana/log/"
     DCONF="/usr/sap/codilog/hana/conf/"
     USERNAME=$(cat $DCONF/CONF | awk -F ':' '{print $1}')
     PASSWORD=$(cat $DCONF/CONF | awk -F ':' '{print $2}')
-    $DBIN/hdbsql -u $USERNAME -p $PASSWORD -n localhost -d DDB -i 02 -I $DBIN/check_backup.sql -o $DLOG/check_backup.log > /dev/null
+    SID=$(pgrep -a "sapstartsrv" | grep HDB | awk -F '/' '{print $4}')
+    SN=$(pgrep -a "sapstartsrv" | grep HDB | awk -F '/' '{print $5}' | awk -F 'HDB' '{print $2}')
+    DEXE="/usr/sap/$SID/HDB$SN/exe/"
+    $DBIN/hdbsql -u $USERNAME -p $PASSWORD -n localhost -d $SID -i $SN -I $DBIN/check_backup.sql -o $DLOG/check_backup.log > /dev/null
     _CR=$?
     if [[ $_CR -eq 0 ]];then
-        state=$(cat /usr/sap/codilog/hana/log/check_backup.log| grep "successful\|failed") > /dev/null
+        state=$(cat /usr/sap/codilog/hana/log/check_backup.log| grep "successful\|failed\|running\|canceled") > /dev/null
         if [[ $state == '"failed"' ]];then 
-            echo "Backup DDB en erreur"
+            echo "Backup $SID en erreur"
             exit 2
         elif [[ $state == '"successful"' ]];then
-            echo "Backup DDB successful"
+            echo "Backup $SID successful"
             exit 0
+        elif [[ $state == '"running"' ]];then
+            echo "Backup $SID successful"
+            exit 0
+        elif [[ $state == '"canceled"' ]];then
+            echo "Backup $SID canceled"
+            exit 2
         fi
     else
         echo "CR = $_CR - Requête en erreur"
